0210c2544da2c11600ef402f4305eb2fd0018e4d
James Maguire <jvm986@gmail.com>
2023-02-15 14:24:10 +0100

    refactor environment variables

--- Makefile
+++ Makefile
@@ -4,4 +4,5 @@ build:
 	sam build
 
 start-api: build
-	sam local start-api
+	sam local start-api \
+	--parameter-overrides $(shell cat .aws-sam/development-params)
--- internal/app/shorten/main.go
+++ internal/app/shorten/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jvm986/url-shortener/internal/pkg/shortener"
 )
 
 func main() {
+	pathLengthString, ok := os.LookupEnv("PATH_LENGTH")
+	if !ok {
+		panic("failed to lookup PATH_LENGTH from environment variables")
+	}
+
+	pathLength, err := strconv.Atoi(pathLengthString)
+	if err != nil {
+		panic(fmt.Sprintf("PATH_LENGTH [%s] is not an integer", pathLengthString))
+	}
+
 	md5Config := shortener.MD5ShortenerConfig{
-		PathLength: 10,
+		PathLength: pathLength,
 	}
 	md5shortener := shortener.NewMD5Shortener(md5Config)
-	shortenHandler := NewShortenHandler(md5shortener, "base/")
+
+	endpoint, ok := os.LookupEnv("ENDPOINT")
+	if !ok {
+		panic("failed to lookup ENDPOINT from environment variables")
+	}
+
+	shortenHandler := NewShortenHandler(md5shortener, endpoint)
 
 	lambda.Start(shortenHandler.handleShorten)
 }
