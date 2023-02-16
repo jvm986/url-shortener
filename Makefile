af5fe21cca8198ae6bbcbd3f1b501f558f8e92c3
James Maguire <jvm986@gmail.com>
2023-02-16 16:29:28 +0100

    configure dynamodb

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@
 build:
 	sam build
 
-start-api: build
+local-init-storage:
+	docker stop dynamodb || true && docker rm dynamodb || true
+	docker run -d -p 8000:8000 --name dynamodb amazon/dynamodb-local
+	aws dynamodb create-table --cli-input-json file://.aws-sam/storage-table.json --endpoint-url http://localhost:8000 2>&1 > /dev/null
+
+start-api: build local-init-storage
 	sam local start-api \
 	--parameter-overrides $(shell cat .aws-sam/development-params)
--- internal/app/shorten/main.go
+++ internal/app/shorten/main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/jvm986/url-shortener/internal/pkg/shortener"
@@ -41,9 +42,37 @@ func main() {
 		panic("failed to lookup STORAGE_TABLE_NAME from environment variables")
 	}
 
-	awsConfig, err := config.LoadDefaultConfig(ctx)
+	env, ok := os.LookupEnv("ENV")
+	if !ok {
+		panic("failed to lookup ENV from environment variables")
+	}
+	dynamodbEndpoint, ok := os.LookupEnv("DDB_ENDPOINT")
+	if !ok {
+		panic("failed to lookup DDB_ENDPOINT from environment variables")
+	}
+
+	awsRegion, ok := os.LookupEnv("REGION")
+	if !ok {
+		panic("failed to lookup REGION from environment variables")
+	}
+
+	dynamodbEndpointResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		if service == dynamodb.ServiceID && env == "development" {
+			return aws.Endpoint{
+				URL:           dynamodbEndpoint,
+				SigningRegion: awsRegion,
+			}, nil
+		}
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+
+	awsConfig, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion(awsRegion),
+		config.WithEndpointResolverWithOptions(dynamodbEndpointResolver),
+	)
 	if err != nil {
-		panic("failed to load default aws config")
+		panic("failed to load aws config")
 	}
 
 	dynamodbClient := dynamodb.NewFromConfig(awsConfig)
